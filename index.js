bf9312d5fd9a49743e2d5bbc369a043ff61f0f20
Willy Tseng <willytseng@exosite.com>
2015-10-29 04:22:56 +0800

    testing koa

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-var http = require('http');
+const Koa = require('koa')
+const app = new Koa()
+const SandCastle = require('sandcastle').SandCastle
 
-var SandCastle = require('sandcastle').SandCastle;
+var sandcastle = new SandCastle()
 
-var sandcastle = new SandCastle();
+const stream = require('stream')
 
-var script = sandcastle.createScript("\
-  exports.main = function() {\
-    exit('Hey ' + name + ' Hello World!');\
-  }\
-");
+// logger
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  script.on('exit', function(err, output) {
-	res.end(output); // Hello World!
-  });
 
-  script.run({name: 'Willy'}); // we can pass variables into run.
+app.use((ctx, next) => {
+	const start = new Date
+	return next().then(() => {
+		const ms = new Date - start
+		console.log(`${ctx.method} ${ctx.url} - ${ms}`)
+	})
+})
 
-}).listen(process.env.PORT || 3000);
+// response
+
+app.use(ctx => {
+	console.log('oh')
+
+	var script = sandcastle.createScript("\
+		exports.main = function() {\
+			exit('Hey ' + name + ' Hello World!');\
+		}\
+	")
+
+	ctx.body = new stream.Readable({
+		read: function(n) {
+			console.log(n)
+			var self = this
+			// sets this._read under the hood
+			script.on('exit', (err, output) => {
+				console.log(output)
+				console.log(self.push(output))
+				console.log(self.push(null))
+				// ctx.body = output
+				// res.end(output); // Hello World!
+			});
+
+			script.run({name: 'Willy'}) // we can pass variables into run.
+		}
+	});
+});
+
+app.listen(process.env.PORT || 3000);
 
 console.log('Server running at http://exo-script.herokuapp.com');
