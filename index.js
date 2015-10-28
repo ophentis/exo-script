2275113e3c624b62cc5f6b94abab8a25ecf17a88
Willy Tseng <willytseng@exosite.com>
2015-10-29 02:43:02 +0800

    fix again

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen((process.env.PORT || 3000);
+}).listen(process.env.PORT || 3000);
 
 console.log('Server running at http://exo-script.herokuapp.com');
