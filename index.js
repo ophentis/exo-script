d70e2df955ad64d842e9395ccc0cba2ee74c0644
Willy Tseng <willytseng@exosite.com>
2015-10-29 02:52:35 +0800

    try sandcastle

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var http = require('http');
 
+var SandCastle = require('sandcastle').SandCastle;
+
+var sandcastle = new SandCastle();
+
+var script = sandcastle.createScript("\
+  exports.main = function() {\
+    exit('Hey ' + name + ' Hello World!');\
+  }\
+");
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+
+  script.on('exit', function(err, output) {
+	res.end(output); // Hello World!
+  });
+
+  script.run({name: 'Willy'}); // we can pass variables into run.
+
 }).listen(process.env.PORT || 3000);
 
 console.log('Server running at http://exo-script.herokuapp.com');
